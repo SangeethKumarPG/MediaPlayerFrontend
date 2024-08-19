af0a5b1e7c5f8a9335599c5df7267a64ce14a56b
SangeethKumarPG <101401368+SangeethKumarPG@users.noreply.github.com>
2024-08-19 08:15:35 +0000

    changing server url

--- src/services/serverUrl.js
+++ src/services/serverUrl.js
@@ -1,2 +1,4 @@
-export const serverURL = "http://localhost:4000";
-export const downloadServerURL = "http://localhost:3000";
+// export const serverURL = "http://localhost:4000";
+export const serverURL = "https://mediaplayerbackend-5ia9.onrender.com"
+// export const downloadServerURL = "http://localhost:3000";
+export const downloadServerURL = "https://mediaplayerdownloader.onrender.com"
